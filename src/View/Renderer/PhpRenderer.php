e5503e1c284821dbe0ae8d9ff1702f8452de0e16
Frank Verhoeven <frankverhoeven@me.com>
2018-01-03 13:27:15 +0100

    Code Completion

---
+++ src/View/Renderer/PhpRenderer.php
@@ -0,0 +1,18 @@
+<?php
+
+use FrankVerhoeven\Bootstrap\View\Helper\Alert;
+use FrankVerhoeven\Bootstrap\View\Helper\DismissibleAlert;
+use Zend\View\Renderer\PhpRenderer as ZendPhpRenderer;
+
+/**
+ * PhpRenderer
+ *
+ * Allow code completion of view helpers.
+ *
+ * @author Frank Verhoeven <hi@frankverhoeven.me>
+ *
+ * @method Alert bootstrapAlert(string $content, array $arguments = null)
+ * @method DismissibleAlert bootstrapDismissibleAlert(string $content, array $arguments = null)
+ */
+class PhpRenderer extends ZendPhpRenderer
+{}
